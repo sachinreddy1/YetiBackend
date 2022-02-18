522e0788457b82c9aee85c371fa37045527ce418
sachinreddy1 <reddysachin2014@gmail.com>
2022-02-17 20:52:26 -0500

    first commit

---
+++ src/main/kotlin/com/sachinreddy/yetibackend/YetibackendApplication.kt
@@ -0,0 +1,11 @@
+package com.sachinreddy.yetibackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YetibackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<YetibackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/sachinreddy/yetibackend/YetibackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sachinreddy.yetibackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YetibackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
